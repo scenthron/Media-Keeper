764885b9b46e406c4fe7c01ef55d85bf11fe27e3
Centhron <scenthron@gmail.com>
2026-07-06 01:49:07 +0300

    chore: miscellaneous bugfixes and ui improvements

--- src/logic_cache.py
+++ src/logic_cache.py
@@ -1,15 +1,15 @@
 
 import os
-import threading
-from PyQt6.QtCore import QObject, pyqtSignal, QThread, QMutex, QWaitCondition
+from PyQt6.QtCore import QObject, pyqtSignal, QRunnable, QThreadPool
 
-class ScanWorker(QThread):
-    # Третий параметр - размер в байтах. Используем object для поддержки чисел > 2GB (signed 32-bit limit)
-    result_ready = pyqtSignal(str, int, object) # path, count, size
+class ScanWorkerSignals(QObject):
+    result_ready = pyqtSignal(str, int, object)
 
+class ScanWorker(QRunnable):
     def __init__(self, path):
         super().__init__()
         self.path = path
+        self.signals = ScanWorkerSignals()
 
     def run(self):
         total_size = 0
@@ -17,7 +17,6 @@ class ScanWorker(QThread):
         from utils_io import ensure_long_path
         long_folder = ensure_long_path(self.path)
         try:
-            # 1. Считаем файлы только в корне самой папки
             try:
                 with os.scandir(long_folder) as it:
                     for entry in it:
@@ -26,7 +25,6 @@ class ScanWorker(QThread):
             except:
                 pass
                 
-            # 2. Обходим подпапки рекурсивно только для подсчета суммарного размера
             for root, dirs, files in os.walk(long_folder):
                 if '.mediakeeper' in dirs:
                     dirs.remove('.mediakeeper')
@@ -37,7 +35,7 @@ class ScanWorker(QThread):
                         total_size += os.path.getsize(fp)
                     except: pass
         except: pass
-        self.result_ready.emit(self.path, file_count, total_size)
+        self.signals.result_ready.emit(self.path, file_count, total_size)
 
 class DirCache(QObject):
     updated = pyqtSignal(str) # path that changed
@@ -47,8 +45,11 @@ class DirCache(QObject):
     def __init__(self):
         super().__init__()
         self._cache = {} # path -> {'c': count, 's': size}
-        self._workers = {} # path -> QThread
-        self._mutex = QMutex()
+        self._active_scans = set() # paths currently being scanned
+        
+        # Dedicated thread pool for disk scanning to prevent IO thrashing
+        self.thread_pool = QThreadPool()
+        self.thread_pool.setMaxThreadCount(1)
 
     @staticmethod
     def inst():
@@ -74,17 +75,16 @@ class DirCache(QObject):
 
     def refresh_path(self, path):
         path = os.path.normpath(path)
-        if path in self._workers: return # Already scanning
+        if path in self._active_scans: return # Already scanning
         
+        self._active_scans.add(path)
         worker = ScanWorker(path)
-        worker.result_ready.connect(self._on_worker_finished)
-        # Безопасная очистка: удаляем из словаря только когда поток ПОЛНОСТЬЮ завершен
-        worker.finished.connect(lambda p=path: self._workers.pop(p, None))
-        self._workers[path] = worker
-        worker.start()
+        worker.signals.result_ready.connect(self._on_worker_finished)
+        self.thread_pool.start(worker)
 
     def _on_worker_finished(self, path, count, size):
         path = os.path.normpath(path)
+        self._active_scans.discard(path)
         self._cache[path] = {'c': count, 's': size}
         self.updated.emit(path)
 
